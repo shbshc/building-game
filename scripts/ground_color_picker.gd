22207e77b7a20f5404b42b282e60d49f51444ee4
Builder <builder@game.local>
2026-06-19 03:05:22 +0800

    fix: scroll wheel properly updates selected color via slot_selected signal

--- scripts/ground_color_picker.gd
+++ scripts/ground_color_picker.gd
@@ -5,25 +5,25 @@ var grid_color: Color
 var target_node: Node
 
 func _ready():
-    $Scroll/VBox/GroundPicker.color_changed.connect(_on_ground_color_changed)
-    $Scroll/VBox/GridPicker.color_changed.connect(_on_grid_color_changed)
-    $Scroll/VBox/Apply.pressed.connect(_on_apply)
+	$Scroll/VBox/GroundPicker.color_changed.connect(_on_ground_color_changed)
+	$Scroll/VBox/GridPicker.color_changed.connect(_on_grid_color_changed)
+	$Scroll/VBox/Apply.pressed.connect(_on_apply)
 
 func open_with_colors(node, gc: Color, gridc: Color):
-    target_node = node
-    ground_color = gc
-    grid_color = gridc
-    $Scroll/VBox/GroundPicker.color = gc
-    $Scroll/VBox/GridPicker.color = gridc
-    popup_centered()
+	target_node = node
+	ground_color = gc
+	grid_color = gridc
+	$Scroll/VBox/GroundPicker.color = gc
+	$Scroll/VBox/GridPicker.color = gridc
+	popup_centered()
 
 func _on_ground_color_changed(c: Color):
-    ground_color = c
+	ground_color = c
 
 func _on_grid_color_changed(c: Color):
-    grid_color = c
+	grid_color = c
 
 func _on_apply():
-    if target_node:
-        target_node.update_colors(ground_color, grid_color)
-    hide()
+	if target_node:
+		target_node.update_colors(ground_color, grid_color)
+	hide()
